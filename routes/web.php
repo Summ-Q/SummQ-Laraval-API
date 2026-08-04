e32fed5557d5da3f6f2df4b1a7584691837e57b9
Ma7moudEGY <mahmoudfatahalla@gmail.com>
2026-08-04 19:41:36 +0300

    moving an api route to web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('api')->group(function () {
+    Route::get('/tests', function () {
+    return response()->json(['message' => 'API is working22!']);
+});
+});
