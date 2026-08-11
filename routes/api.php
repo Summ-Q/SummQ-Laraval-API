339409d72779854cf780ce037ffe9ab627d1014e
Mostafa Ahmed <momoformostafa@gmail.com>
2026-08-11 22:32:17 +0300

    Implement DeckController with index, store, and destroy methods; update routes for deck management

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,9 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DeckController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
 Route::get('/test', function () {
     return response()->json(['message' => 'API is working!']);
@@ -14,4 +11,13 @@ Route::get('/test', function () {
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group(function () {
+    // Authorization
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    // Decks
+    Route::get('/decks', [DeckController::class, 'index']);
+    Route::post('/decks', [DeckController::class, 'store']);
+    Route::delete('/decks/{deck_id}', [DeckController::class, 'destroy']);
+});
